96c9139c71b5b64056ceef4820242c209e9f3a1f
Cole Burton <cole.l.burton@gmail.com>
2016-01-15 20:40:40 -0500

    Added style and ng-hide function

--- listingController.js
+++ listingController.js
@@ -6,35 +6,58 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     //was having trouble with ng-show, should require a global ($scope) variable to start false and become true when a listingis clicked but was not working for me
     //$scope.details = false;
 
+    $scope.details = true;
+    $scope.defaultMessage = false;
+    $scope.addListButton = false;
+    $scope.showAddListing = true;
+
+    $scope.addButton = function(){
+
+          $scope.showAddListing = false;
+          $scope.details = true;
+          $scope.defaultMessage = true;
+          $scope.addListButton = true;
+
+        }
     $scope.addListing = function(entry) {
 
-     $scope.listings.push(entry);
-     $scope.entry = null;
+          $scope.listings.push(entry);
+          $scope.entry = null;
+          $scope.showAddListing = false;
+          $scope.details = true;
+          $scope.defaultMessage = true;
+          $scope.addListButton = true;
 
    };
 
    $scope.deleteListing = function(listing) {
 
-       $scope.listings.splice($scope.listings.indexOf(listing),1);
+          $scope.listings.splice($scope.listings.indexOf(listing),1);
+          $scope.details = true;
+          $scope.defaultMessage = false;
 
    };
 
    $scope.showDetails = function(listing) {
-       $scope.code = '';
-       $scope.building = '';
-       $scope.address = '';
-       $scope.coords = '';
 
-       index = $scope.listings.indexOf(listing);
+          $scope.showAddListing = true;
+          $scope.addListButton = false;
 
-       $scope.code = $scope.listings[index].code;
-       $scope.building = $scope.listings[index].name;
-       $scope.address = $scope.listings[index].address;
-       $scope.coords = $scope.listings[index].coordinates.latitude + ', ' + $scope.listings[index].coordinates.longitude;
+          $scope.details = false;
+          $scope.defaultMessage = true;
 
-       //varible should become true after accessing this function
-       //$scope.details = true;
+          $scope.code = '';
+          $scope.building = '';
+          $scope.address = '';
+          $scope.coords = '';
 
-   };
+          index = $scope.listings.indexOf(listing);
+
+          $scope.code = $scope.listings[index].code;
+          $scope.building = $scope.listings[index].name;
+          $scope.address = $scope.listings[index].address;
+          $scope.coords = $scope.listings[index].coordinates.latitude + ', ' + $scope.listings[index].coordinates.longitude;
+
+      };
   }
 ]);
